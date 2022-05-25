04888072ec65f3c02fe03de8531770ffb31926e0
Remi <remi.barciela@gmail.com>
2022-05-25 10:11:45 -0400

    Tell user if they got it right

--- math_quiz.py
+++ math_quiz.py
@@ -4,4 +4,11 @@ num1 = randint(1,10)
 num2 = randint(1,10)
 
 question = "What is " + str(num1) + " times " + str(num2) + "? "
-answer = int(input(question))
+user_answer = int(input(question))
+correct_answer = num1 * num2
+
+if user_answer == correct_answer:
+    print("That's right - well done.")
+else:
+    print("No, Im adraid the answer is " + str(correct_answer) + ".")
+
