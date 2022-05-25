a18efe99248a727fc48f74ca06c11fa4983e8efe
Remi <remi.barciela@gmail.com>
2022-05-25 10:57:55 -0400

    Tells the user how many questions they got correct

--- math_quiz.py
+++ math_quiz.py
@@ -1,14 +1,20 @@
 from random import randint
 
-num1 = randint(1,10)
-num2 = randint(1,10)
+count = 0
 
-question = "What is " + str(num1) + " times " + str(num2) + "? "
-user_answer = int(input(question))
-correct_answer = num1 * num2
+for _ in range(10):
 
-if user_answer == correct_answer:
-    print("That's right - well done.")
-else:
-    print("No, Im adraid the answer is " + str(correct_answer) + ".")
+    num1 = randint(1,10)
+    num2 = randint(1,10)
 
+    question = "What is " + str(num1) + " times " + str(num2) + "? "
+    user_answer = int(input(question))
+    correct_answer = num1 * num2
+
+    if user_answer == correct_answer:
+        print("That's right - well done.")
+        count += 1
+    else:
+        print("No, Im adraid the answer is " + str(correct_answer) + ".")
+
+print("I asked you 10 questions. You got " + str(count) + " of them right. \nWell done!")
